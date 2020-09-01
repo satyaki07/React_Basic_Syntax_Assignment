e67cd08d5d655e0c9ea605f01f7260628a9e49fa
Satyaki Bose <satyakibose98@gmail.com>
2020-09-01 19:42:25 +0530

    First Commit

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const userOutput = props => {
+    return (
+        <div>
+            <p>
+                Hello I am {props.name}.
+            </p>
+            <p>
+                My name is Satyaki.
+            </p>
+        </div>
+    )
+}
+
+export default userOutput;
